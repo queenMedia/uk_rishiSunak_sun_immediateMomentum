61833d5ac3b0f688705db60f34e574fa117f09ce
anastasiia.mahalias <anastasiia.mahalias@powercode.us>
2024-05-02 13:27:30 +0300

    fix target and script view

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
-const buttons = document.querySelectorAll("button");
-const images = document.querySelectorAll("img");
-const spansInP = document.querySelectorAll("p span");
-const spansInEm = document.querySelectorAll("em span");
-const spansInTitles = document.querySelectorAll("h2 span");
-const comments = document.querySelectorAll(".comments .block");
-const steps = document.querySelectorAll(".results .step");
-
-function goToOfferPage() {
-  window.location.href = "https://trueprofits360.com/immediate_momentum_v2/ca/en/";
-}
-
-buttons.forEach(btn => {
-  btn.addEventListener("click", goToOfferPage);
-});
-images.forEach(img => {
-  img.addEventListener("click", goToOfferPage);
-});
-spansInP.forEach(span => {
-  span.addEventListener("click", goToOfferPage);
-});
-spansInEm.forEach(span => {
-  span.addEventListener("click", goToOfferPage);
-});
-spansInTitles.forEach(span => {
-  span.addEventListener("click", goToOfferPage);
-});
-comments.forEach(comment => {
-  comment.addEventListener("click", goToOfferPage);
-});
-steps.forEach(step => {
-  step.addEventListener("click", goToOfferPage);
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('button');
+  const images = document.querySelectorAll('img');
+  const spansInP = document.querySelectorAll('p span');
+  const spansInEm = document.querySelectorAll('em span');
+  const spansInTitles = document.querySelectorAll('h2 span');
+  const comments = document.querySelectorAll('.comments .block');
+  const steps = document.querySelectorAll('.results .step');
+  
+  function goToOfferPage() {
+    window.open('https://trueprofits360.com/immediate_momentum_v2/ca/en/', '_blank');
+  }
+  
+  const elementsToClick = [
+    ...buttons,
+    ...images,
+    ...spansInP,
+    ...spansInEm,
+    ...spansInTitles,
+    ...comments,
+    ...steps
+  ];
+  
+  elementsToClick.forEach(element => {
+    element.addEventListener('click', goToOfferPage);
+  });
 });
